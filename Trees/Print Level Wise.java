5333364e30eccb721fceec7d09043f39bba2883b
vehaan <vehaanskundra@gmail.com>
2020-06-22 20:32:40 +0530

    Generic Trees

---
+++ Trees/Print Level Wise.java
@@ -0,0 +1,165 @@
+import java.util.Queue;
+import java.util.LinkedList;
+public class Solution {
+
+/*	TreeNode structure 
+ * 
+ * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+	
+	
+	
+	public static void printLevelWise(TreeNode<Integer> root){
+		/* Your class should be named Solution 
+ 		 * Don't write main(). 
+		 * Don't read input, it is passed as function argument. 
+		 * Print output and don't return it. 
+		 * Taking input is handled automatically. 
+		 */
+        Queue<TreeNode<Integer>> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int n = queue.size();
+            while (n>0){
+                TreeNode<Integer> p = queue.peek();
+                queue.poll();
+                System.out.print(p.data + " ");
+                for (int i=0;i<p.children.size();i++){
+                    queue.add(p.children.get(i));
+                }
+                n--;
+            }
+            System.out.println();
+            
+        }
+
+	}
+		
+}
+
+//MAIN
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+ class QueueEmptyException extends Exception {
+	 
+}
+
+ class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T data){
+			this.data = data;
+		}
+	}
+	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException{
+		if(size == 0){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+
+	public void enqueue(T element){
+		Node<T> newNode = new Node<T>(element);
+
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException{
+		if(head == null){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if(head == tail){
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+   class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}
+ 
+public class Main {
+
+	
+
+	static Scanner s = new Scanner(System.in);
+
+	public static TreeNode<Integer> takeInputLevelWise(){
+		QueueUsingLL<TreeNode<Integer>> pendingNodes = new QueueUsingLL<TreeNode<Integer>>();  // Queue of node that are entered themselves but their children aren't added yet
+		int rootData = s.nextInt();
+		TreeNode<Integer> root = new TreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+		while(!pendingNodes.isEmpty()){
+			TreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+				int numChild = s.nextInt();
+				for(int i = 0 ; i < numChild; i++){
+					int currentChild = s.nextInt();
+					TreeNode<Integer> childNode = new TreeNode<Integer>(currentChild);
+					pendingNodes.enqueue(childNode);
+					currentNode.children.add(childNode);
+				}
+			} catch (QueueEmptyException e) {
+			}
+		}
+		return root;
+	}
+
+
+	public static void main(String[] args) {
+		TreeNode<Integer> root =  takeInputLevelWise();
+		Solution.printLevelWise(root); 
+	}
+
+}
